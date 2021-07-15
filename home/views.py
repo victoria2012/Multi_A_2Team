76a22c53c47f868acd43390af68243b6cc0801ed
juni <alicelikesbab@gmail.com>
2021-07-16 01:03:31 +0900

    goorm_push

--- home/views.py
+++ home/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import HttpResponse
+
+def index(request):
+    path=request.path
+    resultstr=''
+    if path =='/home':
+        resultstr='<h1>여기는 home 입니다.</h1>'
+    else:
+        resultstr= '<h1>여기는 main 입니다.</h1>'
+    return HttpResponse(resultstr)
 
 def index(request):
     result = {'first':'Multi',
