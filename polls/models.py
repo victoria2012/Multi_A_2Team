db5a223d52a7d67c5a2369b71a955a54d9f7fc62
victoria2012 <victoria2012@naver.com>
2021-07-20 12:40:43 +0900

    sqlite3 nmovie update

--- polls/models.py
+++ polls/models.py
@@ -6,4 +6,4 @@ class nmovie(models.Model):
     outline = models.CharField(max_length=100)
     director = models.CharField(max_length=100)
     actor = models.CharField(max_length=100)
-    score = models.CharField(max_length=100)
+    score = models.DecimalField(max_digits=20, decimal_places=5, default=0)
