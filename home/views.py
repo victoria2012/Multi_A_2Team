9cd25b439b50b74d8542e3d8e128e6e19922c7e1
juni <alicelikesbab@gmail.com>
2021-07-21 17:54:50 +0900

    test

--- home/views.py
+++ home/views.py
@@ -13,12 +13,19 @@ def index(request):
         resultstr= '<h1>여기는 main 입니다.</h1>'
     return HttpResponse(resultstr)
 
-def index01(request):
-    result = {'first':'Multi',
-              'second':'2Team_project'
-              }
-    return render(request, 'index.html',context=result)
+# def index01(request):
+#     result = {'first':'Multi',
+#               'second':'2Team_project'
+#               }
+#     return render(request, 'index.html',context=result)
 
 def index02(request):
     result = {'first':request.GET['first'],'second':request.GET['second']}
     return render(request, 'index.html',context=result)
+
+def index01(request):
+    mf = folium.Map([35.3369, 127.7306], zoom_start= 10)
+    mf = mf._repr_html_()
+    first = 'hwasa'
+    result = {'mapfolium':mf, 'f01':first}
+    return render(request, templates_name='index.html',context=result)
