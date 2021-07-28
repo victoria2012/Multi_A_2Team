446ab5e9d5c4c7e704a5201b61b629dc08000c0a
juni <alicelikesbab@gmail.com>
2021-07-28 16:53:38 +0900

    스크레핑

--- home/views.py
+++ home/views.py
@@ -10,6 +10,8 @@ def green_home(request):
     return render(request, 'green_home.html', context=result)
 
 def projects(request):
+    df=pd.read_csv('machinelearning/saves/result.csv')
+    list_df = df.values.tolist()
     result={}
     return render(request, 'projects.html', result)
 
@@ -20,8 +22,6 @@ def scrapping(request):
     return render(request, 'scrapping.html', context=result)
 
 def machine(request):
-    # df=pd.read_csv('../machinelearning/saves/result.csv')
-    # list_df = df.values.tolist()
     xArray = [0.46,0.39,0.38,0.35,0.32,0.31,0.29,0.29,0.28,0.27];
     yArray = ['이스케이프 룸 2: 노 웨이 아웃','미드나이트','명탐정 코난: 비색의 탄환','꽃다발 같은 사랑을 했다',
               '라야와 마지막 드래곤','보스 베이비 2','루카','오필리아','컨저링 3: 악마가 시켰다','랑종'];
