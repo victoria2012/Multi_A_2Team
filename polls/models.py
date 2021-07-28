70fbe84611bd5dc7804ec0544c6b1fdfeddd113b
victoria2012 <victoria2012@naver.com>
2021-07-28 09:18:20 +0900

    naver movie DB insert를 위한 작업

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,13 @@ class MovieReview(models.Model):
     code = models.DecimalField(max_digits=20, decimal_places=5, default=0)
     score = models.DecimalField(max_digits=20, decimal_places=5, default=0)
     author = models.CharField(max_length=100)
+
+class NaverMovie(models.Model):
+    code = models.DecimalField(max_digits=20, decimal_places=5, default=False)
+    author = models.CharField(max_length=90)
+    rating = models.DecimalField(max_digits=20, decimal_places=5, default=False)
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    nation = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    actor = models.CharField(max_length=100)
